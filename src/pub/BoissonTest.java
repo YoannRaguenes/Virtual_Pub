a5837caff2645089cf2fe0d0c26b03e946c545fb
YoannRaguenes <yoann.raguenes.29@gmail.com>
2021-03-05 09:55:03 +0100

    tests classe Boisson

--- src/pub/BoissonTest.java
+++ src/pub/BoissonTest.java
@@ -2,13 +2,50 @@ package pub;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
 
-class BoissonTest {
-
+class BoissonTest extends TestCase {
+	private Boisson megaDemon;
+	private Boisson crazyTiger;
+	private Boisson maximator;
+	private Boisson muscatDeRivesaltes;
+	
+	@BeforeEach
+	protected void setUp() {
+	megaDemon = new Boisson("Mega Demon",(float) 16);
+	crazyTiger = new Boisson("Crazy Tiger");
+	maximator = new Boisson("Maximator",(float) -10);
+	muscatDeRivesaltes = new Boisson("MuscatDeRiveslates", (float) 102);
+	
+	}
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void emptyBoisson() {
+		assertNotNull(crazyTiger);	
+	}
+	
+	@Test
+	public void emptyBoisson2() {
+		assertNotNull(megaDemon);
+	}
+	
+	@Test
+	public void limiteBoisson() {
+		assertFalse(maximator.degre>0);
+	}
+	
+	@Test 
+	public void limiteBoisson2() {
+		assertFalse(muscatDeRivesaltes.degre<100);
+	}
+	
+	@Test
+	public void testToString() {
+		assertNotNull(megaDemon.toString());
 	}
 
 }
