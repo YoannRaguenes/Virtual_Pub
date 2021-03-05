8f6afbe5c37f7198f2f7746c14e7f9e6baa8ba2b
YoannRaguenes <yoann.raguenes.29@gmail.com>
2021-03-05 10:50:17 +0100

    tests classe Cave

---
+++ src/pub/CaveTest.java
@@ -0,0 +1,57 @@
+package pub;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+
+class CaveTest extends TestCase {
+	private Cave cave1;
+	private Boisson ricard;
+	private Boisson captainMorgan;
+	
+	@BeforeEach
+	protected void setUp() {
+		cave1 = new Cave();
+		ricard = new Boisson("ricard");
+		captainMorgan = new Boisson("Captain Morgan");
+	}
+
+	@Test
+	public void emptyCave() {
+		assertNotNull(cave1);
+	}
+	
+	@Test
+	public void testAdd() {
+		cave1.add(ricard);
+		assertEquals(cave1.rayons.size(),1);
+	}
+	
+	@Test
+	public void testTake() {
+		cave1.add(captainMorgan);
+		cave1.take("captainMorgan");
+		assertEquals(cave1.rayons.size(),0);
+	}
+	
+	@Test
+	public void limiteTake() {
+		cave1.add(captainMorgan);
+		cave1.add(captainMorgan);
+		cave1.take("ricard");
+		assertEquals(cave1.rayons.size(),2);
+	}
+	
+	@Test
+	public void limiteTake2() {
+		cave1.take("ricard");
+		assertEquals(cave1.rayons.size(),0);
+	}
+	
+	@Test
+	public void testToStringCave() {
+		assertNotNull(cave1.toString());
+	}
+
+}
